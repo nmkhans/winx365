6b41d996dcb79b70bcdb0a6a1e56a960b6bddc5c
nmkhans <nurmoin05@gmail.com>
2023-01-16 21:32:55 +0600

    agent types section added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import AccountInfo from '../../components/Sections/AccountInfo/AccountInfo';
 import AgentInfo from '../../components/Sections/AgentInfo/AgentInfo';
+import AgentType from '../../components/Sections/AgentType/AgentType';
 
 const Home = () => {
     return (
         <>
            <AccountInfo />
            <AgentInfo />
+           <AgentType />
         </>
     );
 };
