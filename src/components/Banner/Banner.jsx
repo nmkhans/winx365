458dba002cc5c8d431716174dab60e3c3994f6af
nmkhans <nurmoin05@gmail.com>
2023-01-16 19:37:38 +0600

    banner added and content layout added

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import banner from "../../assets/banner.jpg"
+import "./Banner.scss";
+
+const Banner = () => {
+    return (
+        <div className="Banner">
+            <div className="container banner__content">
+                <div className="banner__image">
+                    <img src={banner} alt="Banner Image" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
