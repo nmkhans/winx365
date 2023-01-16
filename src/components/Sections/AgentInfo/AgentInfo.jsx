de1ff5b83c65e616bf0d5c1dcd43e104e7459ff5
nmkhans <nurmoin05@gmail.com>
2023-01-16 21:23:12 +0600

    agent info section added

---
+++ src/components/Sections/AgentInfo/AgentInfo.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./AgentInfo.scss";
+
+const AgentInfo = () => {
+    return (
+        <section className="AgentInfo">
+            <div className="container">
+                <div className="agentInfo__content">
+                    <div className="agentInfo__header">
+                        <h3>
+                            এজেন্ট লিস্টঃ
+                        </h3>
+                    </div>
+                    <div className="agentInfo__body">
+                        <p>একাউন্ট খুলতে নিম্নে অনলাইন এজেন্ট লিস্ট এ ক্লিক করুন। এজেন্ট লিস্টে যাদের নাম আছে - তাদের ফেসবুক লিঙ্ক এর উপরে ক্লিক করলে ফেসবুক আইডি আসবে অথবা ফেসবুক আইকন এর উপরে ক্লিক করলে ফেসবুক এপ দিয়ে আইডি ওপেন হবে। এই সব এজেন্ট এর নামে ডুপ্লিকেট বা ফেইক আইডি থাকতে পারে। তাই এই সব এজেন্ট দের নাম ফেইসবুকে সার্চ না করে - লিঙ্ক এ ক্লিক করে যোগাযোগ করুন। অনলাইন মাষ্টার এজেন্ট লিস্টঃ</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default AgentInfo;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import AccountInfo from '../../components/Sections/AccountInfo/AccountInfo';
+import AgentInfo from '../../components/Sections/AgentInfo/AgentInfo';
 
 const Home = () => {
     return (
         <>
            <AccountInfo />
+           <AgentInfo />
         </>
     );
 };
